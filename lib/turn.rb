4595573889fe109d3cf03865e56848b815aca02f
Chris Baum <christopher.baum@valpo.edu>
2020-12-29 00:49:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,5 +23,6 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  input_to_index(user_input)
+  #input_to_index(user_input)
+  valid_move?(board, input_to_index(user_input))
 end
